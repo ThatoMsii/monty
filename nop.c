79d7c64cbd103a28a2678a90f496d96ea8d849bd
Thato <thatomsimanga@gmail.com>
2023-12-14 03:04:04 -0800

    updated monty files

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_nop - doesn't do anything
+ * @head: stack head
+ * @counter: line counter
+ *
+ * Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
